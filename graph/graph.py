5159f7576179ddd6383ed55ad751381e04d8dee5
shirin <75791599+shirin1996@users.noreply.github.com>
2022-03-30 15:18:01 +0430

    readme graph + implementation graph code updated

--- graph/graph.py
+++ graph/graph.py
@@ -1,10 +1,11 @@
 #Graph representation
-
 class Graph:
     def __init__(self, gdict=None):
         if gdict is None:
             gdict = {}
         self.gdict = gdict
-        
+
     def addedge(self, vertex, edge):
         self.gdict[vertex].append(edge)
+
+#Algo to be added...
