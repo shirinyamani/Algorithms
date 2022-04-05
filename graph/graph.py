ffe1679c92e04b65a0ef80fc221191c78e4d7b02
shirin <75791599+shirin1996@users.noreply.github.com>
2022-04-05 13:34:01 +0430

    DFS code added to graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -18,3 +18,15 @@ class Graph:
                 if adjecntdeVertex not in visited:
                     visited.append(adjecntdeVertex)
                     queue.append(adjecntdeVertex)
+
+    #DFS
+    def dfs(self, vertex):
+        stack = [vertex]
+        visited = [vertex]
+        while stack:
+            popVertex = stack.pop() #bring last element
+            for adjecentVertex in self.gdict[popVertex]:
+                if adjecentVertex not in visited:
+                    visited.append(adjecentVertex)
+                    stack.append(adjecentVertex)
+
