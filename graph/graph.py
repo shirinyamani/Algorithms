aee87f59b6e8f459da8b930aba90bfe58483ab6e
shirin <75791599+shirin1996@users.noreply.github.com>
2022-04-05 12:58:25 +0430

    BFS code added to graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -8,4 +8,13 @@ class Graph:
     def addedge(self, vertex, edge):
         self.gdict[vertex].append(edge)
 
-#Algo to be added...
+    #BFS
+    def bfs(self, vertex):
+        queue = [vertex]
+        visited = [vertex]
+        while queue:
+            deVertex = queue.pop(0) #works as the queue (FIFO)
+            for adjecntdeVertex in self.gdict[deVertex]:
+                if adjecntdeVertex not in visited:
+                    visited.append(adjecntdeVertex)
+                    queue.append(adjecntdeVertex)
